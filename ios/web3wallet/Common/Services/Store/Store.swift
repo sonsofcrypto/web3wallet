6d7c81390274a41ab689971093293fff8119d53f
web3d3v <shadowyWeb3SuperCoder@protonmail.com>
2022-02-13 17:19:19 -0300

    Adding `AnyCodable` to store
    Adding `AnyCodable` to store

--- ios/web3wallet/Common/Services/Store/Store.swift
+++ ios/web3wallet/Common/Services/Store/Store.swift
@@ -11,51 +11,39 @@ protocol Store {
 
 class DefaultStore {
 
-    private lazy var coder = JSONSerialization()
+    private lazy var encoder = JSONEncoder()
+    private lazy var decoder = JSONDecoder()
+
     private lazy var url: URL = try! FileManager.default.url(
         for: .documentationDirectory,
         in: .userDomainMask,
         appropriateFor: nil,
         create: true
     ).appendingPathComponent("store.json")
-    
-    private var store: [String: Any] = [:]
 }
 
 // MARK: - Store
 extension DefaultStore: Store {
 
     func set<T>(_ val: T, key: String) throws where T: Codable {
-//        createStoreIfNeeded()
-//        let data = try Data(contentsOf: url)
-//        let json = try JSONSerialization.jsonObject(
-//            with: data,
-//            options: .fragmentsAllowed
-//        )
-//
-//        guard var dict = json as? Dictionary<String, Decodable> else {
-//            throw StoreError.failedToReadStoreData
-//        }
-//
-//        dict[key] = val
-//        let newData = try JSONSerialization.data(withJSONObject: dict)
-//        try newData.write(to: url, options: .atomic)
-        store[key] = val
+        createStoreIfNeeded()
+        let data = try Data(contentsOf: url)
+        var json = try decoder.decode([String: AnyCodable].self, from: data)
+        json[key] = AnyCodable(val)
+
+        let newData = try encoder.encode(json)
+        try newData.write(to: url, options: .atomic)
     }
 
     func get<T>(_ key: String) -> T? where T: Codable {
-//        createStoreIfNeeded()
-//        guard let data = try? Data(contentsOf: url) else {
-//            return nil
-//        }
-//
-//        let json = try? JSONSerialization.jsonObject(
-//            with: data,
-//            options: .fragmentsAllowed
-//        )
-//
-//        return (json as? [String: Decodable])?[key] as? T
-        return store[key] as? T
+        createStoreIfNeeded()
+
+        guard let data = try? Data(contentsOf: url) else {
+            return nil
+        }
+
+        var json = try? decoder.decode([String: AnyCodable].self, from: data)
+        return json?[key]?.value as? T
     }
 
     func createStoreIfNeeded() {
