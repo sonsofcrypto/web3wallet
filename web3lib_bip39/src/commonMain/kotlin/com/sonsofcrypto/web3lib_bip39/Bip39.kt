0ab01f0fdb5342b3f4c3b2820892646c5bd84236
web3d3v <shadowyWeb3SuperCoder@protonmail.com>
2022-05-27 20:21:32 -0300

    Fixing build

--- web3lib_bip39/src/commonMain/kotlin/com/sonsofcrypto/web3lib_bip39/Bip39.kt
+++ web3lib_bip39/src/commonMain/kotlin/com/sonsofcrypto/web3lib_bip39/Bip39.kt
@@ -1,14 +1,14 @@
 package com.sonsofcrypto.web3lib_bip39
 
-class Bip39 {
-
-}
-
-enum WordList {
-    case
-
-}
-
-enum class WordList {
-    ENGLISH, SOUTH, WEST, EAST
-}
+//class Bip39 {
+//
+//}
+//
+//enum WordList {
+//    case
+//
+//}
+//
+//enum class WordList {
+//    ENGLISH, SOUTH, WEST, EAST
+//}
