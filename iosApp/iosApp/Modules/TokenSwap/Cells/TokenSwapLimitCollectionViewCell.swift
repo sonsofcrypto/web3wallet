a30bd977e109fb47bea13ec0182f268c4c18349b
web3d4v <105275520+web3d4v@users.noreply.github.com>
2022-07-21 18:06:15 +0200

    Feature/add coming soon (#31)
    * Update TokenSend save to show under construction alert
    
    * Add coming soon to swap limit orders

--- iosApp/iosApp/Modules/TokenSwap/Cells/TokenSwapLimitCollectionViewCell.swift
+++ iosApp/iosApp/Modules/TokenSwap/Cells/TokenSwapLimitCollectionViewCell.swift
@@ -8,6 +8,39 @@ final class TokenSwapLimitCollectionViewCell: UICollectionViewCell {
         
         super.awakeFromNib()
 
-        backgroundColor = .blue
+        let imageView = UIImageView(image: .init(named: "coming-soon-meme"))
+        addSubview(imageView)
+        
+        let wrapperView = UIView()
+        wrapperView.backgroundColor = .clear
+        wrapperView.addSubview(imageView)
+        
+        imageView.addConstraints(
+            [
+                .layout(anchor: .widthAnchor, constant: .equalTo(constant: 112)),
+                .layout(anchor: .heightAnchor, constant: .equalTo(constant: 112)),
+                .layout(anchor: .topAnchor),
+                .layout(anchor: .bottomAnchor),
+                .layout(anchor: .centerXAnchor)
+            ]
+        )
+        
+        let titleLabel = UILabel()
+        titleLabel.apply(style: .largeTitle)
+        titleLabel.textAlignment = .center
+        titleLabel.text = Localized("comingSoon").uppercased()
+        
+        let vStack = VStackView([wrapperView, titleLabel])
+        vStack.spacing = Theme.constant.padding
+        
+        addSubview(vStack)
+        
+        vStack.addConstraints(
+            [
+                .layout(anchor: .leadingAnchor, constant: .equalTo(constant: Theme.constant.padding)),
+                .layout(anchor: .trailingAnchor, constant: .equalTo(constant: Theme.constant.padding)),
+                .layout(anchor: .topAnchor, constant: .equalTo(constant: Theme.constant.padding * 2))
+            ]
+        )
     }
 }
