fcaeb708efbaafb1cef9be7322cdfdd5b7fde135
web3d4v <105275520+web3d4v@users.noreply.github.com>
2022-07-27 22:50:32 +0200

    Add FeatureService to the app

---
+++ iosApp/iosApp/Services/Features/FeaturesServiceAssembler.swift
@@ -0,0 +1,16 @@
+// Created by web3d4v on 27/07/2022.
+// Copyright (c) 2022 Sons Of Crypto.
+// SPDX-License-Identifier: MIT
+
+import Foundation
+
+final class FeaturesServiceAssembler: AssemblerComponent {
+    
+    func register(to registry: AssemblerRegistry) {
+        
+        registry.register(scope: .instance) { _ -> FeaturesService in
+            
+            DefaultFeaturesService()
+        }
+    }
+}
