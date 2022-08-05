b6f80b66cde19679499f419dd434a6abb14374f4
web3d3v <shadowyWeb3SuperCoder@protonmail.com>
2022-08-05 17:33:18 +0300

    fixing buttons

--- iosApp/iosApp/Modules/Dashboard/Cells/DashboardButtonsCell.swift
+++ iosApp/iosApp/Modules/Dashboard/Cells/DashboardButtonsCell.swift
@@ -6,51 +6,38 @@ import UIKit
 
 final class DashboardButtonsCell: UICollectionViewCell {
 
-    private weak var lineView: UIView!
     @IBOutlet weak var receiveButton: Button!
     @IBOutlet weak var sendButton: Button!
     @IBOutlet weak var tradeButton: Button!
+    @IBOutlet weak var stack: UIStackView!
     
     private weak var presenter: DashboardPresenter!
     private var viewModel: [DashboardViewModel.Action] = []
 
     override func awakeFromNib() {
-        
         super.awakeFromNib()
-        
-        let view = UIView()
-        addSubview(view)
-        view.addConstraints(
-            [
-                .layout(anchor: .leadingAnchor),
-                .layout(anchor: .trailingAnchor),
-                .layout(anchor: .heightAnchor, constant: .equalTo(constant: 0.3)),
-                .layout(anchor: .topAnchor)
-            ]
-        )
-        self.lineView = view
-        
+        stack.spacing = Theme.constant.padding
         updateViews()
     }
     
     override func traitCollectionDidChange(
         _ previousTraitCollection: UITraitCollection?
     ) {
-        
         super.traitCollectionDidChange(previousTraitCollection)
-        
         updateViews()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+//        backgroundColor = .red
     }
 }
 
 private extension DashboardButtonsCell {
     
     func updateViews() {
-
         viewModel.forEach {
-            
             switch $0.type {
-                
             case .receive:
                 receiveButton.style = .dashboardAction(
                     leftImage: $0.imageName.assetImage?.withTintColor(Theme.colour.labelPrimary)
@@ -68,35 +55,30 @@ private extension DashboardButtonsCell {
                 tradeButton.setTitle($0.title, for: .normal)
             }
         }
-        lineView.backgroundColor = Theme.colour.labelPrimary
     }
 }
 
 extension DashboardButtonsCell {
     
     @IBAction func receiveTapped() {
-        
         presenter.handle(.receiveAction)
     }
 
     @IBAction func sendTapped() {
-        
         presenter.handle(.sendAction)
     }
 
     @IBAction func tradeTapped() {
-        
         presenter.handle(.swapAction)
     }
 }
 
+// MARK: - ViewModel
+
 extension DashboardButtonsCell {
 
     func update(with viewModel: [DashboardViewModel.Action], presenter: DashboardPresenter) {
-        
-        self.presenter = presenter
         self.viewModel = viewModel
-        
         updateViews()
     }
 }
