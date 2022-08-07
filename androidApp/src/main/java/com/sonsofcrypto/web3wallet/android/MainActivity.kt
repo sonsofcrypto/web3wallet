a168b64303663e33364b583a9369ca49aa4d58df
web3d3v <shadowyWeb3SuperCoder@protonmail.com>
2022-08-07 07:56:01 +0300

    downloading colors for currencies

--- androidApp/src/main/java/com/sonsofcrypto/web3wallet/android/MainActivity.kt
+++ androidApp/src/main/java/com/sonsofcrypto/web3wallet/android/MainActivity.kt
@@ -3,16 +3,22 @@ package com.sonsofcrypto.web3wallet.android
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.sonsofcrypto.web3lib.services.currencyMetadata.BundledAssetProviderApplication
 import com.sonsofcrypto.web3lib.utils.secureRand
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        BundledAssetProviderApplication.setInstance(application)
+
         setContentView(R.layout.activity_main)
 
         val tv: TextView = findViewById(R.id.text_view)
         tv.text = secureRand(128).toString()
+
+
+
     }
 
     override fun onResume() {
@@ -25,9 +31,9 @@ class MainActivity : AppCompatActivity() {
 //        ProviderTest().runAll()
 //        CoinGeckoTest(this.applicationContext).runAll()
 //        Web3ServiceTests().runAll()
-//        CurrenciesServiceTests().runAll()
+        CurrenciesServiceTests().runAll()
 //        WalletStateTest().runAll()
-        CurrencyFormatterTest().runAll()
+//        CurrencyFormatterTest().runAll()
         println("=== all tests executed ===")
     }
 }
