4e903375ab7a181384ddfc4cf357d1feccdfb9f2
web3d3v <shadowyWeb3SuperCoder@protonmail.com>
2022-08-19 09:52:20 +0300

    Fixing subList crash

--- web3lib/src/commonMain/kotlin/com/sonsofcrypto/web3lib/utils/extensions/ListExtensions.kt
+++ web3lib/src/commonMain/kotlin/com/sonsofcrypto/web3lib/utils/extensions/ListExtensions.kt
@@ -4,5 +4,7 @@ import kotlin.math.min
 
 /** Returns sublist to index. If index is greater than list returns full list */
 inline fun <E>List<out E>.subListTo(index: Int): List<E> {
+    if (this.isEmpty())
+        return this
     return this.subList(0, min(index, this.size - 1))
 }
