a80a0a47b335fc5778eebdbdc8fc5c8544dc89cd
web3d4v <105275520+web3d4v@users.noreply.github.com>
2022-08-31 17:54:57 +0200

    W3W-32 Use english locale on currency formatting

---
+++ iosApp/iosApp/Common/Extensions/Locale+Extensions.swift
@@ -0,0 +1,7 @@
+// Created by web3d4v on 31/08/2022.
+// Copyright (c) 2022 Sons Of Crypto.
+// SPDX-License-Identifier: MIT
+
+extension Locale {
+    static var english: Locale { .init(identifier: "en" ) }
+}
