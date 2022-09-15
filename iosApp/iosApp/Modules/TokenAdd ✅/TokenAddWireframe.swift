f96ee563ece4dcd7f14723e20a089062b3f7382f
web3d4v <105275520+web3d4v@users.noreply.github.com>
2022-09-15 11:11:17 +0200

    W3W-101 Refactor TokenAdd module ✅

--- iosApp/iosApp/Modules/TokenAdd ✅/TokenAddWireframe.swift
+++ iosApp/iosApp/Modules/TokenAdd ✅/TokenAddWireframe.swift
@@ -21,7 +21,7 @@ enum TokenAddWireframeDestination {
 // MARK: TokenAddWireframe
 
 protocol TokenAddWireframe {
-    func present()
+    func show()
     func navigate(to destination: TokenAddWireframeDestination)
     func dismiss()
 }
@@ -54,7 +54,7 @@ final class DefaultTokenAddWireframe {
 
 extension DefaultTokenAddWireframe: TokenAddWireframe {
     
-    func present() {
+    func show() {
         let vc = wireUp()
         parent?.show(vc, sender: self)
     }
