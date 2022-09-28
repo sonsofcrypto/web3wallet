a007742f0550e67b3ff2d2b939283cb3e83bc170
Marcus Sjölin <marcus@marcussjolin.com>
2022-09-28 11:47:45 +0200

    W3W-96 - Started adding small suite of tests for QuantityDataHexString

---
+++ web3lib/src/commonTest/kotlin/com/sonsofcrypto/web3lib/provider/model/QuantityDataHexStringTest.kt
@@ -0,0 +1,30 @@
+package com.sonsofcrypto.web3lib.provider.model
+
+import com.sonsofcrypto.web3lib.utils.BigInt
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class QuantityDataHexStringTest {
+    @Test
+    fun testQuantitiesLong() {
+        val actual = QuantityHexString(10000000000)
+        assertEquals("0x2540be400", actual)
+    }
+    @Test
+    fun testQuantitiesUInt() {
+        val actual = QuantityHexString(10000000000u)
+        assertEquals("0x2540be400", actual)
+    }
+    @Test
+    fun testQuantitiesBigInt() {
+        val actual = QuantityHexString(BigInt.Companion.from(10000000000))
+        assertEquals("0x2540be400", actual)
+    }
+
+
+    @Test
+    fun testDataHexString() {
+        val actual = DataHexString(ByteArray(10))
+        assertEquals("0x00000000000000000000", actual)
+    }
+}
