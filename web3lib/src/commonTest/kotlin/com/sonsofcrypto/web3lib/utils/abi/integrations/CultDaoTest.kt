7eab3a590e1b15dc31677df2831908fc2285b8fd
Marcus Sjölin <marcus@marcussjolin.com>
2022-10-02 09:10:56 +0200

    W3W-96 Fix test to make sure it is rescillient to changes

--- web3lib/src/commonTest/kotlin/com/sonsofcrypto/web3lib/utils/abi/integrations/CultDaoTest.kt
+++ web3lib/src/commonTest/kotlin/com/sonsofcrypto/web3lib/utils/abi/integrations/CultDaoTest.kt
@@ -1,12 +1,14 @@
 package com.sonsofcrypto.web3lib.utils.abi.integrations
 
 import com.sonsofcrypto.web3lib.utils.BigInt
+import com.sonsofcrypto.web3lib.utils.setReturnValue
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class CultDaoTest {
     @Test
     fun testCultDaoDeposit() {
+        setReturnValue("e2bbb158ea830e9efa91fa2a38c9708f9f6109a6c571d6a762b53a83776a3d67")
         val expected = "0xe2bbb15800000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004840aa3e5e8b722426e6eb"
         val actual = CultDao.deposit(
             BigInt.from(0),
