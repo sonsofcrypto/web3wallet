893619ecd7eeb7ac6613c9d689f3c1621d6505c6
web3d3v <shadowyWeb3SuperCoder@protonmail.com>
2022-10-05 09:47:00 +0300

    fixing localization

--- web3walletcore/src/iosMain/kotlin/com/sonsofcrypto/web3walletcore/extensions/LocalizeExtensions.kt
+++ web3walletcore/src/iosMain/kotlin/com/sonsofcrypto/web3walletcore/extensions/LocalizeExtensions.kt
@@ -1,13 +1,18 @@
 package com.sonsofcrypto.web3walletcore.extensions
 
 import platform.Foundation.NSString
+import platform.Foundation.NSValue
 import platform.Foundation.localizedStringWithFormat
 import platform.Foundation.stringWithFormat
+import platform.darwin.NSObject
 
 actual fun Localized(string: String): String {
     return NSString.localizedStringWithFormat(string, null)
 }
 
 actual fun LocalizedFmt(fmt: String, vararg params: Any): String {
-    return NSString.stringWithFormat(Localized(fmt), params)
+    return NSString.stringWithFormat(
+        Localized(fmt),
+        params.map { it as NSObject }
+    )
 }
