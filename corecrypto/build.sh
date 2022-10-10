21fed03165f88488da58491bd4854bb12a01628f
web3d3v <shadowyWeb3SuperCoder@protonmail.com>
2022-10-10 13:33:58 +0300

    cleaning up build script

--- corecrypto/build.sh
+++ corecrypto/build.sh
@@ -1,49 +1,42 @@
 #!/bin/zsh
 
-#Android
-rm corecrypto.aar
-rm corecrypto-sources.jar
-gomobile bind -v -target=android -o corecrypto.aar ./
+# Android
+rm ./build/android/coreCrypto.aar
+rm ./build/android/coreCrypto-sources.jar
+gomobile bind -v -target=android -o ./build/android/coreCrypto.aar ./
 
-#iOS
+# iOS
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  gomobile bind -v -target=ios -o ./CoreCrypto.xcframework ./
+  gomobile bind -v -target=ios -o ./build/ios/CoreCrypto.xcframework ./
 
+  # Replace import syntax so that KMM can handle it
   SRC="@import Foundation;"
   DST="#import <Foundation\/Foundation.h>"
 
   sed -i -- "s/$SRC/$DST/g" \
-    ./CoreCrypto.xcframework/ios-arm64/CoreCrypto.framework/Headers/CoreCrypto.objc.h
+    ./build/ios/CoreCrypto.xcframework/ios-arm64/CoreCrypto.framework/Headers/CoreCrypto.objc.h
 
   sed -i -- "s/$SRC/$DST/g" \
-    ./CoreCrypto.xcframework/ios-arm64/CoreCrypto.framework/Headers/Universe.objc.h
+    ./build/ios/CoreCrypto.xcframework/ios-arm64/CoreCrypto.framework/Headers/Universe.objc.h
 
   sed -i -- "s/$SRC/$DST/g" \
-    ./CoreCrypto.xcframework/ios-arm64_x86_64-simulator/CoreCrypto.framework/Headers/CoreCrypto.objc.h
+    ./build/ios/CoreCrypto.xcframework/ios-arm64_x86_64-simulator/CoreCrypto.framework/Headers/CoreCrypto.objc.h
 
   sed -i -- "s/$SRC/$DST/g" \
-    ./CoreCrypto.xcframework/ios-arm64_x86_64-simulator/CoreCrypto.framework/Headers/Universe.objc.h
+    ./build/ios/CoreCrypto.xcframework/ios-arm64_x86_64-simulator/CoreCrypto.framework/Headers/Universe.objc.h
 
-  rm ./CoreCrypto.xcframework/ios-arm64/CoreCrypto.framework/Headers/CoreCrypto.objc.h--
-  rm ./CoreCrypto.xcframework/ios-arm64/CoreCrypto.framework/Headers/Universe.objc.h--
-  rm ./CoreCrypto.xcframework/ios-arm64_x86_64-simulator/CoreCrypto.framework/Headers/CoreCrypto.objc.h--
-  rm ./CoreCrypto.xcframework/ios-arm64_x86_64-simulator/CoreCrypto.framework/Headers/Universe.objc.h--
+  # Remove renaming artifact files
+  rm ./build/ios/CoreCrypto.xcframework/ios-arm64/CoreCrypto.framework/Headers/CoreCrypto.objc.h--
+  rm ./build/ios/CoreCrypto.xcframework/ios-arm64/CoreCrypto.framework/Headers/Universe.objc.h--
+  rm ./build/ios/CoreCrypto.xcframework/ios-arm64_x86_64-simulator/CoreCrypto.framework/Headers/CoreCrypto.objc.h--
+  rm ./build/ios/CoreCrypto.xcframework/ios-arm64_x86_64-simulator/CoreCrypto.framework/Headers/Universe.objc.h--
 
-  rm -rf ./../web3lib/src/iosMain/libs/CoreCrypto/ios-arm64/CoreCrypto.framework
-  rm -rf ./../web3lib/src/iosMain/libs/CoreCrypto/ios-arm64_x86_64-simulator/CoreCrypto.framework
+  rm -rf ./build/ios/ios-arm64/CoreCrypto.framework
+  rm -rf ./build/ios/ios-arm64_x86_64-simulator/CoreCrypto.framework
 
-  cp -r ./CoreCrypto.xcframework/ios-arm64/CoreCrypto.framework \
-    ./../web3lib/src/iosMain/libs/CoreCrypto/ios-arm64/CoreCrypto.framework
-
-  cp -r ./CoreCrypto.xcframework/ios-arm64_x86_64-simulator/CoreCrypto.framework \
-    ./../web3lib/src/iosMain/libs/CoreCrypto/ios-arm64_x86_64-simulator/CoreCrypto.framework
-
-  rm -rf ./../web3walletcore/src/iosMain/libs/CoreCrypto/ios-arm64/CoreCrypto.framework
-  rm -rf ./../web3walletcore/src/iosMain/libs/CoreCrypto/ios-arm64_x86_64-simulator/CoreCrypto.framework
-
-  cp -r ./CoreCrypto.xcframework/ios-arm64/CoreCrypto.framework \
-    ./../web3walletcore/src/iosMain/libs/CoreCrypto/ios-arm64/CoreCrypto.framework
-
-  cp -r ./CoreCrypto.xcframework/ios-arm64_x86_64-simulator/CoreCrypto.framework \
-    ./../web3walletcore/src/iosMain/libs/CoreCrypto/ios-arm64_x86_64-simulator/CoreCrypto.framework
+  # Split framework by architecture and move to dep files folder
+  cp -r ./build/ios/CoreCrypto.xcframework/ios-arm64/CoreCrypto.framework \
+    ./build/ios/ios-arm64/CoreCrypto.framework
+  cp -r ./build/ios/CoreCrypto.xcframework/ios-arm64_x86_64-simulator/CoreCrypto.framework \
+    ./build/ios/ios-arm64_x86_64-simulator/CoreCrypto.framework
 fi
