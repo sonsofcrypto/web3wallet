4c5f8672e1617516a0613f2666803307c3528eb3
web3d3v <shadowyWeb3SuperCoder@protonmail.com>
2022-10-10 15:44:39 +0300

    W3W-${} Minor clean up

--- web3walletcore/src/commonMain/kotlin/com/sonsofcrypto/web3walletcore/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.sonsofcrypto.web3walletcore
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
--- web3walletcore/src/iosMain/kotlin/com/sonsofcrypto/web3walletcore/Platform.kt
+++
@@ -1,9 +0,0 @@
-package com.sonsofcrypto.web3walletcore
-
-import platform.UIKit.UIDevice
-
-class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
-}
-
-actual fun getPlatform(): Platform = IOSPlatform()
