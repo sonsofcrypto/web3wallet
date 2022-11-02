7ed8c6205ad1bc73c00adad4de1825866f92ac7d
web3d4v <105275520+web3d4v@users.noreply.github.com>
2022-11-02 11:55:20 +0100

    W32-182-Switch off logs

--- web3walletcore/src/commonMain/kotlin/com/sonsofcrypto/web3walletcore/services/nfts/NFTsService.kt
+++ web3walletcore/src/commonMain/kotlin/com/sonsofcrypto/web3walletcore/services/nfts/NFTsService.kt
@@ -59,7 +59,7 @@ class OpenSeaNFTsService(
     private var listeners: MutableList<NFTsServiceListener> = mutableListOf()
     private val client: HttpClient by lazy {
         HttpClient {
-            Logging { level = LogLevel.INFO; logger = Logger.SIMPLE }
+            Logging { level = LogLevel.NONE; logger = Logger.SIMPLE }
             install(ContentNegotiation) { json(stdJson, Json.withCharset(Charsets.UTF_8)) }
         }
     }
