912a9a8e4573e78843adb5a6165d0e760a93976f
web3d3v <shadowyWeb3SuperCoder@protonmail.com>
2022-11-25 20:10:00 +0200

    Updating tests

--- web3lib/src/commonTest/kotlin/com/sonsofcrypto/web3lib/Resource.kt
+++
@@ -1,8 +0,0 @@
-package com.sonsofcrypto.web3lib
-
-expect class Resource(name: String) {
-    val name: String
-
-    fun exists(): Boolean
-    fun readText(): String
-}
--- web3lib/src/commonTest/kotlin/com/sonsofcrypto/web3lib/contract/InterfaceTests.kt
+++ web3lib/src/commonTest/kotlin/com/sonsofcrypto/web3lib/contract/InterfaceTests.kt
@@ -1,6 +1,5 @@
 package com.sonsofcrypto.web3lib.contract
 
-import com.sonsofcrypto.web3lib.Resource
 import com.sonsofcrypto.web3lib.formatters.Formatters
 import com.sonsofcrypto.web3lib.types.Currency
 import com.sonsofcrypto.web3lib.utils.bip39.Bip39
@@ -13,9 +12,10 @@ class InterfaceTests {
 
     @Test
     fun testMAXEthereum1() {
-        val res = Resource("contractsJson/IERC20.json").exists()
+        println("=== STARTING COMMON TESTs")
+//        val res = Resource("contractsJson/IERC20.json").exists()
         println(Bip39.from(Bip39.EntropySize.ES128).mnemonic)
-        println("=== Exits $res")
+//        println("=== Exits $res")
 //        val output = maxOutput("21000000000000000043", Currency.ethereum())
 //        assertEquals(listOf(Formatters.Output.Normal("21.000000000000000043 ETH")), output)
     }
