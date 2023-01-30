bb02e9aba176a5cc8f3ef1c0e517c3674df7d059
web3d4v <105275520+web3d4v@users.noreply.github.com>
2023-01-30 14:30:47 +0100

    Fix build

--- androidApp/src/main/java/com/sonsofcrypto/web3wallet/android/common/Bootstrapper.kt
+++ androidApp/src/main/java/com/sonsofcrypto/web3wallet/android/common/Bootstrapper.kt
@@ -3,8 +3,9 @@ package com.sonsofcrypto.web3wallet.android.common
 import androidx.appcompat.app.AppCompatActivity
 import com.sonsofcrypto.web3wallet.android.assembler
 import com.sonsofcrypto.web3wallet.android.modules.dashboard.DashboardWireframeFactoryAssembler
-import com.sonsofcrypto.web3wallet.android.modules.root.RootAssembler
+import com.sonsofcrypto.web3wallet.android.modules.degen.DegenWireframeFactoryAssembler
 import com.sonsofcrypto.web3wallet.android.modules.root.RootWireframeFactory
+import com.sonsofcrypto.web3wallet.android.modules.root.RootWireframeFactoryAssembler
 import com.sonsofcrypto.web3wallet.android.services.*
 import com.sonsofcrypto.web3wallet.android.services.keychain.KeyChainServiceAssembler
 
@@ -40,8 +41,9 @@ private class AssemblerBootstrapper: Bootstrapper {
                 WalletServiceAssembler(),
                 NodeServiceAssembler(),
                 // Modules
-                RootAssembler(),
-                DashboardWireframeFactoryAssembler()
+                RootWireframeFactoryAssembler(),
+                DashboardWireframeFactoryAssembler(),
+                DegenWireframeFactoryAssembler(),
             )
         )
     }
