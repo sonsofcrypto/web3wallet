26c70b63ec6ee3e9efc7e58f84f9e7618712e526
web3d4v <105275520+web3d4v@users.noreply.github.com>
2023-03-09 19:06:10 +0100

    Prepare to boot services WIP

--- androidApp/src/main/java/com/sonsofcrypto/web3wallet/android/MainActivity.kt
+++ androidApp/src/main/java/com/sonsofcrypto/web3wallet/android/MainActivity.kt
@@ -4,20 +4,30 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import com.sonsofcrypto.web3lib.services.currencyStore.CurrencyStoreService
+import com.sonsofcrypto.web3lib.services.wallet.WalletService
 import com.sonsofcrypto.web3lib.utils.BundledAssetProviderApplication
+import com.sonsofcrypto.web3lib.utils.bgDispatcher
 import com.sonsofcrypto.web3wallet.android.common.*
+import com.sonsofcrypto.web3wallet.android.modules.compose.currencysend.CurrencySendWireframeFactory
 import com.sonsofcrypto.web3walletcore.extensions.App
+import com.sonsofcrypto.web3walletcore.modules.dashboard.DashboardInteractorEvent
+import com.sonsofcrypto.web3walletcore.services.nfts.NFTsService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import smartadapter.internal.extension.name
 
 val assembler: Assembler = DefaultAssembler()
 lateinit var appContext: Context
 
 class MainActivity : App() {
 
+    private val bgScope = CoroutineScope(bgDispatcher)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         appContext = this
-
         BundledAssetProviderApplication.setInstance(application)
 
         //AppTheme.value = themeMiamiSunriseLight//themeMiamiSunriseDark
@@ -32,8 +42,29 @@ class MainActivity : App() {
             MainBootstrapper(this).boot()
         }
 
+        // bootServices()
+
         //val tv: TextView = findViewById(R.id.text_view)
         //tv.text = secureRand(128).toString()
+    }
+
+    private fun bootServices() {
+        val walletService: WalletService = assembler.resolve(WalletService::class.name)
+        val currencyStoreService: CurrencyStoreService = assembler.resolve(
+            CurrencyStoreService::class.name
+        )
+        val nftsService: NFTsService = assembler.resolve(NFTsService::class.name)
+        val allCurrencies = walletService.networks()
+            .map { walletService.currencies(it) }
+            .reduce { total, it -> total + it }
+
+        if (allCurrencies.isEmpty()) { return }
+
+        bgScope.launch {
+            currencyStoreService.fetchMarketData(allCurrencies)
+            nftsService.fetchNFTs()
+        }
+
     }
 
     override fun onResume() {
