0e73fbe4f7c7c1122bfc6965b8b55bfcc32c540c
web3d3v <shadowyWeb3SuperCoder@protonmail.com>
2023-04-13 16:59:07 -0300

    Moving Trie tests

--- web3lib/src/commonTest/kotlin/com/sonsofcrypto/web3lib/TrieTest.kt
+++ web3lib/src/commonTest/kotlin/com/sonsofcrypto/web3lib/TrieTest.kt
@@ -1,34 +1,27 @@
-package com.sonsofcrypto.web3wallet.android
+package com.sonsofcrypto.web3lib
 
 import com.sonsofcrypto.web3lib.utils.Trie
 import com.sonsofcrypto.web3lib.utils.bip39.WordList
 import com.sonsofcrypto.web3lib.utils.bip39.words
 import com.sonsofcrypto.web3lib.utils.extensions.toHexString
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.encodeToByteArray
 import kotlinx.serialization.protobuf.ProtoBuf
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class TrieTest {
 
-    fun runAll() {
-        testWordSearch()
-    }
-
-    fun assertTrue(actual: Boolean, message: String? = null) {
-        if (!actual) throw Exception("Failed $message")
-    }
-
+    @OptIn(ExperimentalSerializationApi::class)
+    @Test
     fun testWordSearch() {
         val trie = Trie()
-
-        WordList.ENGLISH.words().forEach {
-            trie.insert(it)
-        }
-
-        val bytes = ProtoBuf.encodeToByteArray(trie)
-        val bytesString = bytes.toHexString()
+        WordList.ENGLISH.words().forEach { trie.insert(it) }
+        ProtoBuf.encodeToByteArray(trie).toHexString()
         val result = trie.wordsStartingWith("ag")
-        assertTrue(
-            result == listOf("again", "age", "agent", "agree"),
+        assertEquals(
+            result,
+            listOf("again", "age", "agent", "agree"),
             "Trie search epic fail, trie implementation broken"
         )
     }
