81aacb00d6b4c32a92e78e305fb87e89b4b7a439
web3d3v <shadowyWeb3SuperCoder@protonmail.com>
2023-05-26 12:59:27 -0300

    Setting up wallet service tests for new pooling

---
+++ web3lib/src/commonTest/kotlin/com/sonsofcrypto/web3lib/WalletServiceTests2.kt
@@ -0,0 +1,52 @@
+package com.sonsofcrypto.web3lib
+
+import com.sonsofcrypto.web3lib.keyValueStore.KeyValueStore
+import com.sonsofcrypto.web3lib.services.coinGecko.DefaultCoinGeckoService
+import com.sonsofcrypto.web3lib.services.currencyStore.DefaultCurrencyStoreService
+import com.sonsofcrypto.web3lib.services.currencyStore.ethereumDefaultCurrencies
+import com.sonsofcrypto.web3lib.services.keyStore.DefaultKeyStoreService
+import com.sonsofcrypto.web3lib.services.networks.DefaultNetworksService
+import com.sonsofcrypto.web3lib.services.node.DefaultNodeService
+import com.sonsofcrypto.web3lib.services.wallet.DefaultWalletService
+import com.sonsofcrypto.web3lib.services.wallet.WalletService
+import com.sonsofcrypto.web3lib.types.Network
+
+class WalletServiceTests2 {
+
+}
+
+fun testWalletService(): WalletService {
+    var currencyStoreService = DefaultCurrencyStoreService(
+        DefaultCoinGeckoService(),
+        KeyValueStore("WalletServiceTest.marketStore"),
+        KeyValueStore("WalletServiceTest.candleStore"),
+        KeyValueStore("WalletServiceTest.metadataStore"),
+        KeyValueStore("WalletServiceTest.userCurrencyStore"),
+    )
+    val keyStoreService = DefaultKeyStoreService(
+        KeyValueStore("WalletServiceTest.keyStore"),
+        KeyStoreTest.MockKeyChainService()
+    )
+    keyStoreService.selected = mockKeyStoreItem
+    val networksService = DefaultNetworksService(
+        KeyValueStore("web3serviceTest"),
+        keyStoreService,
+        DefaultNodeService()
+    )
+    var walletService = DefaultWalletService(
+        networksService,
+        currencyStoreService,
+        KeyValueStore("WalletServiceTest.currencies"),
+        KeyValueStore("WalletServiceTest.networkState"),
+        KeyValueStore("WalletServiceTest.transferLogCache"),
+    )
+    walletService.setCurrencies(ethereumDefaultCurrencies, Network.ethereum())
+    walletService = DefaultWalletService(
+        networksService,
+        currencyStoreService,
+        KeyValueStore("WalletServiceTest.currencies"),
+        KeyValueStore("WalletServiceTest.networkState"),
+        KeyValueStore("WalletServiceTest.transferLogCache"),
+    )
+    return walletService
+}
