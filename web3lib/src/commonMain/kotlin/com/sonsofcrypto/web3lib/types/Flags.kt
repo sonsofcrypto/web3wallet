c42dd6359ec828caef48734cea6d65461c75038a
web3d3v <shadowyWeb3SuperCoder@protonmail.com>
2023-12-16 21:59:45 -0300

    Meta and Market cache loading

---
+++ web3lib/src/commonMain/kotlin/com/sonsofcrypto/web3lib/types/Flags.kt
@@ -0,0 +1,9 @@
+package com.sonsofcrypto.web3lib.types
+
+class Flags {
+    companion object {
+        const val migratedCurrencies: Boolean = true
+        const val migratedMetadata: Boolean = true
+        const val migratedMarkets: Boolean = true
+    }
+}
