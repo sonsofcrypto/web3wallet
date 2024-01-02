aebddfc03401420f74b32b86267a65dcf24b843e
web3d3v <shadowyWeb3SuperCoder@protonmail.com>
2024-01-02 12:57:00 -0300

    Deploy transaction address crash fix

--- web3lib/src/commonMain/kotlin/com/sonsofcrypto/web3lib/formatters/NetworkAddressFormatter.kt
+++ web3lib/src/commonMain/kotlin/com/sonsofcrypto/web3lib/formatters/NetworkAddressFormatter.kt
@@ -10,6 +10,9 @@ class NetworkAddressFormatter {
         network: Network = Network.ethereum()
     ): String {
         val l = address.length
+        if (l == 0) {
+            return "0x"
+        }
         return when (network.name.lowercase()) {
             "ethereum" -> {
                 address.dropLast(l - (2 + digits)) + "..." + address.drop(l - digits)
