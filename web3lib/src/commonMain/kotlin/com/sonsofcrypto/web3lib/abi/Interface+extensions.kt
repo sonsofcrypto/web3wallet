3a5c739da9f89ca44bcc2929feec5e89b0259bfc
web3d3v <shadowyWeb3SuperCoder@protonmail.com>
2024-01-12 10:47:01 -0300

    Tests clean up

--- web3lib/src/commonMain/kotlin/com/sonsofcrypto/web3lib/abi/Interface+extensions.kt
+++ web3lib/src/commonMain/kotlin/com/sonsofcrypto/web3lib/abi/Interface+extensions.kt
@@ -1,6 +1,7 @@
 package com.sonsofcrypto.web3lib.abi
 
 import com.sonsofcrypto.web3lib.utils.FileManager
+import com.sonsofcrypto.web3lib.utils.FileManager.Location.BUNDLE
 
 @Throws(Throwable::class)
 fun Interface.Companion.named(name: String): Interface = fromJson(
@@ -9,7 +10,7 @@ fun Interface.Companion.named(name: String): Interface = fromJson(
 
 fun Interface.Companion.ERC20(): Interface {
     cache.get("IERC20")?.let { return it }
-    val iStr = FileManager().readSync("contracts/IERC20.json").decodeToString()
+    val iStr = FileManager().readSync("contracts/IERC20.json", BUNDLE).decodeToString()
     val intf = fromJson(iStr)
     cache["IERC20"] = intf
     return intf
@@ -18,7 +19,7 @@ fun Interface.Companion.ERC20(): Interface {
 fun Interface.Companion.Multicall3(): Interface {
     cache.get("Multicall3")?.let { return it }
     val fm = FileManager()
-    val iStr = fm.readSync("contracts/Multicall3.json").decodeToString()
+    val iStr = fm.readSync("contracts/Multicall3.json", BUNDLE).decodeToString()
     val intf = fromJson(iStr)
     cache["Multicall3"] = intf
     return intf
